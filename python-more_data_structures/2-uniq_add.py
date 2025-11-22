dac594b42b49424f3b90c38d48bf8c2e76465712
Royamhr <12558@holbertonstudents.com>
2025-11-22 11:28:39 +0000

    shhshs

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    new_list=[]
-    for i in my_list:
-        if my_list.count(i) == 1:
-            new_list.append(i)
-    return mew_list
+    uniq_list = []
+    total = 0
+
+    for num in my_list:
+        if num not in uniq_list:
+            uniq_list.append(num)
+            total += num
+
+    return total
