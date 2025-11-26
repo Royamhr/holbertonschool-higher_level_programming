929af8cbcebeb62c8c29b61493f6732968456fd1
Royamhr <12558@holbertonstudents.com>
2025-11-26 12:17:33 +0000

    sjsh

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""Module that defines MyList class, inheriting from list."""
+"""
+aaa
+"""
+
 
 class MyList(list):
-    """MyList class that inherits from list and adds print_sorted method."""
+    """
+    inherits from list
+    """
 
     def print_sorted(self):
-        """Print the list in ascending order without modifying the original list."""
         print(sorted(self))
