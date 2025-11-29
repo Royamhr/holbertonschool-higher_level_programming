9422008eae5f632a20d7e5bf0ba4a22e8917b79a
Roya Meherremli <ryamaharram@gmail.com>
2025-11-29 12:44:13 +0400

    task0

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,12 @@
+import json
+
+def serialize_and_save_to_file(data, filename):
+    # Your code here to serialize and save data to the specified file
+    with open(filename, 'w', encoding='utf-8') as f:
+        result = json.dump(data, f)
+
+def load_and_deserialize(filename):
+    # Your code here to load and deserialize data from the specified file
+    with open(filename, 'r', encoding='utf-8') as f:
+        result = json.load(f)
+        return result
