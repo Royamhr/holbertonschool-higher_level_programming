3cc44d6e1a0ae2c4526c8df38a95292954bc576f
Royamhr <12558@holbertonstudents.com>
2025-11-30 17:09:48 +0000

    sjjs

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -68,9 +68,10 @@ class Rectangle:
 
     def __repr__(self):
         """Return a string representation to recreate a new instance."""
-        return "Rectangle({}, {})".format(self.width, self.height)
+        return f"Rectangle({self.width}, {self.height})"
 
     def __del__(self):
         """Print a message when an instance is deleted."""
         print("Bye rectangle...")
         type(self).number_of_instances -= 1
+
